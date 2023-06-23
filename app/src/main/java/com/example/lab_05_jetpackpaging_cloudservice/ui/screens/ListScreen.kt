1a746f13cc13b923e474d227589c648ed2ca1d66
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-06-22 23:34:13 -0500

    (ADD) | Screen Title composable

--- app/src/main/java/com/example/lab_05_jetpackpaging_cloudservice/ui/screens/ListScreen.kt
+++ app/src/main/java/com/example/lab_05_jetpackpaging_cloudservice/ui/screens/ListScreen.kt
@@ -1,11 +1,34 @@
 package com.example.lab_05_jetpackpaging_cloudservice.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.lab_05_jetpackpaging_cloudservice.ui.theme.ListTitle
+import com.example.lab_05_jetpackpaging_cloudservice.ui.theme.SizeLarge
+import com.example.lab_05_jetpackpaging_cloudservice.util.composables.AppHeader
+import com.example.lab_05_jetpackpaging_cloudservice.util.composables.ScreenTitle
 
 @Composable
 fun ListScreen(navController: NavHostController) {
-    Text(text = "ListScreen")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AppHeader(navController = navController)
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(SizeLarge),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ScreenTitle(title = ListTitle)
+        }
+    }
 
 }
--- app/src/main/java/com/example/lab_05_jetpackpaging_cloudservice/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/lab_05_jetpackpaging_cloudservice/ui/screens/RegisterScreen.kt
@@ -1,10 +1,32 @@
 package com.example.lab_05_jetpackpaging_cloudservice.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.lab_05_jetpackpaging_cloudservice.ui.theme.RegisterTitle
+import com.example.lab_05_jetpackpaging_cloudservice.ui.theme.SizeLarge
+import com.example.lab_05_jetpackpaging_cloudservice.util.composables.AppHeader
+import com.example.lab_05_jetpackpaging_cloudservice.util.composables.ScreenTitle
 
 @Composable
 fun RegisterScreen(navController: NavHostController) {
-    Text(text = "RegisterScreen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AppHeader(navController = navController)
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(SizeLarge),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ScreenTitle(title = RegisterTitle)
+        }
+    }
 }
